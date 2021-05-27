4b1fec1d5ebb7335750e8422b7ef388f330bb685
Hoyoon <hoyunb@gmail.com>
2021-05-28 01:09:00 +0900

    code dup

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ def helloworld(a):
     print(msg)  # adsf
     return msg
 
+def helloworld(a):
+    msg = f"Hello World! {a}"
+    print(msg)  # adsf
+    return msg
+
 
 if __name__ == "__main__":  # pragma: no cover
     helloworld("Deepdog")
