50f46bd013037a10afcc02b7d3d9b09a4a21eb5b
Hoyoon <hoyunb@gmail.com>
2021-05-28 00:26:19 +0900

    add unitest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def helloworld(a):
-    print(f"Hello World! {a}")  # adsf
-
+    msg = f"Hello World! {a}"
+    print(msg)  # adsf
+    return msg
 
 if __name__ == "__main__":
     helloworld("Deepdog")
